0f556f1ce1cf5adaefe186ed332b1a61ee653ede
Songchen Xue <sx2618@nyu.edu>
2026-04-03 23:17:37 +0000

    Update the bootstraper, and build the embedding index for candidates retrival.

--- bootstraper/run_all.sh
+++ bootstraper/run_all.sh
@@ -3,5 +3,6 @@ set -e
 
 python scripts/ingest_datasets.py
 python scripts/build_embedding_text.py
-python scripts/emdding.py
+python scripts/embedding.py
+python scripts/build_embedding_index.py
 python scripts/build_initial_user.py --config scripts/config.yaml
